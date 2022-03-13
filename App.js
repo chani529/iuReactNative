12e108a299a73a52bfbbc83046c178e981ac9af3
chani529 <zdzc529@gmail.com>
2022-03-14 01:24:29 +0900

    test

--- App.js
+++ App.js
@@ -8,44 +8,84 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import {SafeAreaView, StyleSheet, Text, ImageBackground} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  ImageBackground,
+  Image,
+  View,
+  ScrollView,
+} from 'react-native';
 import {Header, Icon} from 'react-native-elements';
+import {Dimensions} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
 const App: () => Node = () => {
   return (
-    <SafeAreaView>
-      <Header
-        leftComponent={{icon: 'menu', color: '#fff'}}
-        centerComponent={{text: 'MY TITLE', style: {color: '#fff'}}}
-        rightComponent={{icon: 'home', color: '#fff'}}
-      />
-      <ImageBackground
-        source={require('./assets/img/img_iu_1920.jpg')}
-        style={styles.bgImage}></ImageBackground>
-    </SafeAreaView>
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+      stickyHeaderIndices={[0]}>
+      <SafeAreaView>
+        {
+          <Header
+            style={styles.headerStyle}
+            leftComponent={{icon: 'menu', color: '#fff'}}
+            centerComponent={{text: 'MY TITLE', style: {color: '#fff'}}}
+            rightComponent={{icon: 'home', color: '#fff'}}
+          />
+        }
+        {/* <View style={styles.tmp}></View> */}
+        {/* <LinearGradient
+          colors={['#4c669f', '#3b5998', '#192f6a']}
+          style={styles.linearGradient}>
+          <Text style={styles.buttonText}>Sign in with Facebook</Text>
+        </LinearGradient> */}
+        {
+          <Image
+            source={require('./assets/img/img_iu_1920.jpg')}
+            style={styles.bgImage}></Image>
+        }
+        {
+          <Image
+            source={require('./assets/img/img_iu_1920.jpg')}
+            style={styles.bgImage}></Image>
+        }
+      </SafeAreaView>
+    </ScrollView>
   );
 };
-
+let ScreenHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
-  bgImage: {
-    width: '150%',
-    height: '100%',
-    // resizeMode: 'stretch',
-    resizeMode: 'cover',
-    // justifyContent: 'center',
+  tmp: {
+    height: 1000,
   },
-  appTitle: {
-    color: '#fff',
-    fontSize: 36,
-    marginTop: 30,
-    marginBottom: 30,
-    fontWeight: '300',
+  headerStyle: {},
+  buttonText: {
+    fontSize: 18,
+    fontFamily: 'Gill Sans',
     textAlign: 'center',
-    backgroundColor: '#3143e8',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
   },
-  container: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: '#3143e8',
+  linearGradient: {
+    zIndex: 1,
+    position: 'absolute',
+    top: -150,
+    left: -50,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5,
+    height: 300,
+    width: 500,
+    borderRadius: 730,
+  },
+  bgImage: {
+    height: ScreenHeight - 100,
+    aspectRatio: 1, // Your aspect ratio
+    // flex: 1,
   },
 });
 
